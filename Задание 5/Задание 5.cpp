22957faaba9b233bc2e3957679761a67db16c063
Andrey Kandybo <87312623+andreywhite01@users.noreply.github.com>
2022-04-17 10:38:24 +0700

    Добавьте файлы проекта.

---
+++ Задание 5/Задание 5.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+    ifstream fin;
+    fin.open("input.txt");
+    int n;
+    fin >> n;
+    fin.close();
+
+    int number = 0;
+    for (int i = 1; i <= n; ++i) {
+        number += i * i;
+    }
+    ofstream fout;
+    fout.open("output.txt");
+    fout << number;
+}
